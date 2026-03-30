d5a8233bff93afbc948b0696177e769de7ee236d
Fernando Gabriel <bielblacksouza@gmail.com>
2026-03-30 18:11:02 -0300

    Bug FIX
    Luiz cagou com o trabalho mas eu e o Danilo arrumamos

--- BancoDedados.sql
+++ BancoDedados.sql
@@ -1,2 +1,19 @@
-create database;
-use database;
+create database DonationBook;
+use DonationBook;
+
+CREATE TABLE livro (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    autor VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    ano_publicacao INT,
+    quantidade_paginas INT,
+    edicao INT,
+    idioma VARCHAR(50),
+    genero VARCHAR(100),
+    condicao VARCHAR(50)
+);
+
+create table Admin (
+
+);
